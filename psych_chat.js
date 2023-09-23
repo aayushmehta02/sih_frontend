21cc1285143472d98c73735071d1eb5ec58f77ff
Viraj Bhartiya <vlbhartiya@gmail.com>
2023-09-23 19:20:20 +0530

    chat backend + refactored file structure

--- psych_chat.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu")
-const main_chat = document.querySelector(".main_chat")
-menu.addEventListener("click",()=>{
-    console.log("hi")
-    main_chat.innerText = "AAYUSH"
-})
