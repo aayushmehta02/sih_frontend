4de09124f8b50f488e41597f2c0e0b26eade9fa3
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2023-09-23 17:39:33 +0530

    new features

---
+++ psych_chat.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu")
+const main_chat = document.querySelector(".main_chat")
+menu.addEventListener("click",()=>{
+    console.log("hi")
+    main_chat.innerText = "AAYUSH"
+})
